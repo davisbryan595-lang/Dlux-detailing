cd8b1957d97a5169479eb7cde8e217f19f7673a8
Wasif <rwasif280@gmail.com>
2025-09-30 05:14:25 +0500

    Update gallery.tsx

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -4,16 +4,16 @@ import Image from "next/image"
 
 const gallery = [
   {
-    before: "https://images.unsplash.com/photo-1603690439501-098e996b5841?auto=format&fit=crop&w=1920&q=80",
-    after: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?auto=format&fit=crop&w=1920&q=80",
-  },
-  {
     before: "https://images.unsplash.com/photo-1568605114967-8130f3a36994?auto=format&fit=crop&w=1920&q=80",
     after: "https://images.unsplash.com/photo-1619767886558-efdc50d4b786?auto=format&fit=crop&w=1920&q=80",
   },
   {
     before: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?auto=format&fit=crop&w=1920&q=80",
     after: "https://images.unsplash.com/photo-1615732045431-4c3f1e8efb1c?auto=format&fit=crop&w=1920&q=80",
+  },
+  {
+    before: "https://images.unsplash.com/photo-1603690439501-098e996b5841?auto=format&fit=crop&w=1920&q=80",
+    after: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?auto=format&fit=crop&w=1920&q=80",
   },
 ]
 
